9cc47f5c0871bb95245ec7fc27f82fb2ecd6a9a7
[vivek kumar] <[vivekmisha140@gmail.com]>
2023-04-10 11:50:22 +0530

    start

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Flickart Clone</title>
+    <link rel="icon" type="image/x-icon" href="https://static-assets-web.flixcart.com/www/promos/new/20150528-140547-favicon-retina.ico">
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <?php include 'navbar.php';?>
+   
+  </body>
+</html>
